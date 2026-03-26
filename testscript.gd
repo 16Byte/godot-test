c9e6cc93bdc0c951575f64f54abb63dc0df7935f
Navi <pixeldevs.diego@gmail.com>
2026-03-26 00:36:28 -0600

    Add basic player and tile set

--- testscript.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var my_int : int = 5;
-
-func _ready() -> void:
-	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-	
-func _process(delta: float) -> void:
-	print(Engine.get_frames_per_second())
